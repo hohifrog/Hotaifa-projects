4b8d87c3173c3517f13eb1c3df1658d144767232
hohifrog <hohironyanai@gmail.com>
2023-03-15 20:58:47 +0200

    Added a "win next move"

--- TicTacToe.py
+++ TicTacToe.py
@@ -5,8 +5,8 @@ def print_board(board):
     print(board[3] + '|' + board[4] + '|' + board[5])
     print('-+-+-')
     print(board[6] + '|' + board[7] + '|' + board[8])
+# ----------------------------------------------------------------------------------------------------------------------
 
-#-----------------------------------------------------------------------------------------------------------------------
 #All options to win tic tac toe
 def check_win(board, player):
     if board[0] == player and board[1] == player and board[2] == player:
@@ -27,8 +27,8 @@ def check_win(board, player):
         return True
     else:
         return False
+# ----------------------------------------------------------------------------------------------------------------------
 
-#-----------------------------------------------------------------------------------------------------------------------
 def tic_tac_toe():
     board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
     players = ['\033[33m' + 'O','\033[91m' +  'X']
@@ -40,17 +40,34 @@ def tic_tac_toe():
         # Determine whose turn it is.
         player = players[turn % 2]
         print("It's " + player + "'s turn.")
-        # Prompt the player to make a move.
-        while True:
-            move = input('Enter a number between 1 and 9: ')
-            # Validate the player's move input.
-            if move.isdigit() and int(move) in range(1, 10):
-                if board[int(move) - 1] != 'X' and board[int(move) - 1] != 'O':
+        # Check for winning moves.
+        winning_moves = []
+        for i in range(9):
+            if board[i] == ' ':
+                board[i] = player
+                if check_win(board, player):
+                    winning_moves.append(i)
+                board[i] = ' '
+        # If there are winning moves, prompt the player to choose one.
+        if winning_moves:
+            print('You can win with move(s):', ', '.join(str(move + 1) for move in winning_moves))
+            while True:
+                move = input('Enter a winning move: ')
+                if move.isdigit() and int(move) - 1 in winning_moves:
                     break
                 else:
-                    print('That space is already taken.')
-            else:
-                print('Invalid input. Please enter a number between 1 and 9.')
+                    print('Invalid input. Please enter a winning move.')
+        # Otherwise, prompt the player to make a move as before.
+        else:
+            while True:
+                move = input('Enter a number between 1 and 9: ')
+                if move.isdigit() and int(move) in range(1, 10):
+                    if board[int(move) - 1] != 'X' and board[int(move) - 1] != 'O':
+                        break
+                    else:
+                        print('That space is already taken.')
+                else:
+                    print('Invalid input. Please enter a number between 1 and 9.')
         # Update the board with the player's move.
         board[int(move) - 1] = player
         # Check if the player has won the game.
@@ -66,6 +83,4 @@ def tic_tac_toe():
         # If neither a win nor a tie has occurred, continue the game with the next player's turn.
         turn += 1
     print('Thanks for playing!')
-
-# Call the main function to start the game.
 tic_tac_toe()
